9bd1afae7688ebdfd1c8d173b369be6f8a0469f0
Verma, Manish <manish.verma2805@gmail.com>
2016-03-17 20:32:48 +0530

    updated to get form

--- routes/index.js
+++ routes/index.js
@@ -62,5 +62,85 @@ router.param('personalinfo', function(req, res, next, id) {
 	  });
 	});
 
+router.get('/personalinfoform', function(req, res, next) {
+	var personalinfoform={
+		"formID":"personalinfo",
+		"formType":"dataEntry",
+		"formTitle":"Personal Info Form",
+		"formSubTitle":"Let's get you started",
+		"fields":[
+			{	"fieldName":"fname",
+				"fieldCaption":"First Name",
+				"fieldType":"alpha",
+				"validation":[
+					{
+						"validationName":"MinMaxLength",
+						"minLength":2,
+						"maxLength":30,
+						"errorMessage":"Enter a string of 2 to 30 char length"
+					},
+					{
+						"validationName":"NotEmpty",
+						"errorMessage":"First Name cannot be empty"
+					}
+				]
+			},
+			{	"fieldName":"lname",
+				"fieldCaption":"Last Name",
+				"fieldType":"alpha",
+				"validation":[
+					{
+						"validationName":"MinMaxLength",
+						"minLength":2,
+						"maxLength":30,
+						"errorMessage":"Enter a string of 2 to 30 char length."
+					}
+				]
+			},
+			{	"fieldName":"dob",
+				"fieldCaption":"Date of birth",
+				"fieldType":"date",
+				"validation":[
+					{
+						"validationName":"cannotBeFutureDate",
+						"errorMessage":"Are you from future?"
+					},
+					{
+						"validationName":"minDate",
+						"minDate":"1900/01/01",
+						"errorMessage":"You are too old."
+					}
+				]
+			},
+			{	"fieldName":"gender",
+				"fieldCaption":"Gender",
+				"fieldType":"singleSelect",
+				"options":["Male","Female","Other"]
+			}
+		],
+		"controls":[
+			{
+				"action":"save",
+				"caption":"Save",
+				"response":[
+					{
+						"resposeType":"success",
+						"message":"Record saved successfully"
+					},
+					{
+						"resposeType":"failure",
+						"message":"Record saved failed"
+					}
+				]
+			},
+			{
+				"action":"cancel",
+				"caption":"Cancel"
+			}
+		]
+	};
+	res.json(personalinfoform);
+});
+
 
 module.exports = router;
